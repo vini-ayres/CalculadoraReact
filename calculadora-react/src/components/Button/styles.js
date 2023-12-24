dfc558d2b4b77633ac07fc428d94186877b6b3ce
vini-ayres <v.ayres@aluno.ifsp.edu.br>
2023-12-24 13:12:07 -0300

    Finalização do desafio

---
+++ calculadora-react/src/components/Button/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 10px;
+    color: 1px solid #cdcdcd;
+    background-color: #00aaf0;
+    color: #ffffff;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+    &:hover {
+        opacity: 0.6;
+    }
+`
